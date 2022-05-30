e74b2d82ff6223bf26819429c09eff856aa6ede0
Kunj <kunj.kshatrainfotech@gmail.com>
2022-05-30 18:33:58 +0530

    30-05-2022

--- Pandas/Task 01.py
+++ Pandas/Task 01.py
@@ -12,14 +12,10 @@ import pandas as pd
 # df3 = pd.read_csv("nba.csv",index_col="Team")
 # print(df3,"\n")
 # print(df3["Name"],"\n") # According to column name
-
 # print(df3.loc[["Boston Celtics","Utah Jazz"]],"\n") # according to row header
 # print(df3.loc[["Boston Celtics","Utah Jazz"],["Age", "Height",  "Weight"]],"\n") # according to row header
-
 # print(df3.iloc[[0,1,-1]])
 # print(df3.iloc[[0,1,-1],[0,2,6]])
-
-# print(df3.ix["Boston Celtics"])
 # print(df3.head(10))
 # print(df3.tail(10))
 
@@ -47,11 +43,27 @@ import pandas as pd
 # for i in df5:
 # 	print(df5[i])
 
-dict4 = {'First Score':[99, 85, 74, 96],
-		'Second Score': [30, 86, 56, 49],
+
+dict4 = {'First Score':[99, 85, 85, 96],
+		'Second Score': [45, 86, 56, 49],
 		'Third Score':[57, 40, 85, None]}
 df6 = pd.DataFrame(dict4)
 df6.insert(3,'Fourth Score',value=[45,56,78,98])
 print(df6)
-print(df6.add(1,fill_value=52))
+print(df6.query('"First Score" < "Second Score"'))
+# print(df6.add(1,fill_value=52))
+# print(df6.sub(1))
+# print(df6.mul(2))
+# print(df6.div(2))
+# print(df6.nunique())
+# print(df6.astype(float))
+# print(df6.sort_values('First Score'))
+# print(df6.sort_values(['First Score','Second Score','Third Score']))
+# print(df6.rename(columns={'First Score':1,'Second Score':2,'Third Score':3,'Fourth Score':4}))
+# print(df6.shape)
+# print(df6.ndim)
+# print(df6.rank())
 
+# list1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# sr1 = pd.Series(list1)
+# print(sr1)
